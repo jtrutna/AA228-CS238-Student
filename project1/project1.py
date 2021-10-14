7304c47c04f4a93a16f93021d14708fa345d4512
jint <jesse@spire.com>
2021-10-13 23:19:57 -0700

    Works on toy example, but there's a bug in m_ijk construction seen on small dataset

--- project1/project1.py
+++ project1/project1.py
@@ -1,4 +1,5 @@
 import argparse
+from random import randrange
 import sys
 
 import networkx as nx
@@ -41,7 +42,7 @@ def evidence_counts_by_ijk(n, G, r_i, D):
             k = row[i]
             j = 0
             ps = parents(i, G)
-            if len(ps) > 1:
+            if len(ps) > 0:
                 parent_values = [row[i] for i in parents(i, G)]
                 parent_rs = [r_i[i] for i in parents(i, G)]
                 # parential instantiations of X_i can be thought of a w-dimensional matrix where w
@@ -52,7 +53,58 @@ def evidence_counts_by_ijk(n, G, r_i, D):
     return m_ijk
 
 
-    # TODO: 
+def bayescore_for_i(m_jk, a_jk):
+    score = np.sum(loggamma(np.sum(a_jk, axis=1)))
+    score -= np.sum(loggamma(np.sum(a_jk, axis=1) + np.sum(m_jk, axis=1)))
+    score += np.sum(loggamma(a_jk + m_jk))
+    score -= np.sum(loggamma(a_jk))
+    return score
+
+
+def bayscore(n, G, r_i, D):
+    a_ijk = prior(n, G, r_i)
+    # TODO: Check for order of columns
+    m_ijk = evidence_counts_by_ijk(n, G, r_i, D)
+    return sum(bayescore_for_i(m_ijk[i], a_ijk[i]) for i in range(n))
+
+
+def _random_neighbor(n, orig_G, r_i, D):
+    G = orig_G.copy()
+    i = randrange(n)
+    j = randrange(n)
+    while j == i:
+        j = randrange(n)
+    if G.has_edge(i, j):
+        G.remove_edge(i, j)
+        if bool(randrange(1)):
+            G.add_edge(j, i)
+    else:
+        G.add_edge(i, j)
+    return G
+
+
+def _has_cycle(G):
+    try:
+        nx.algorithms.cycles.find_cycle(G)
+        return True
+    except nx.exception.NetworkXNoCycle:  # :|
+        return False
+
+
+def fit(n, G, r_i, D):
+    current_score = bayscore(n, G, r_i, D)
+    for _ in range(10):
+        print(f"{G.edges()}:{current_score}")
+        candidate_G = _random_neighbor(n, G, r_i, D)
+        if _has_cycle(candidate_G):
+            continue
+        score = bayscore(n, candidate_G, r_i, D)
+        if score > current_score:
+            G, current_score = candidate_G, score
+
+    return G
+
+
 def compute(infile, outfile):
     D_raw = pd.read_csv(infile, index_col=False, dtype="category")
     n = len(D_raw.columns)
@@ -66,22 +118,10 @@ def compute(infile, outfile):
 
     G = nx.DiGraph()
     G.add_nodes_from(range(n))
-    G.add_edge(1,0)
 
-    a_ijk = prior(n, G, r_i)
-    # TODO: Check for order of columns
-    m_ijk = evidence_counts_by_ijk(n, G, r_i, D)
+    Gp = fit(n, G, r_i, D)
 
-    score = sum(bayescore_for_i(m_ijk[i], a_ijk[i]) for i in range(n))
-    return score
-
-
-def bayescore_for_i(m_jk, a_jk):
-    score = np.sum(loggamma(np.sum(a_jk, axis=1)))
-    score -= np.sum(loggamma(np.sum(a_jk, axis=1) + np.sum(m_jk, axis=1)))
-    score += np.sum(loggamma(a_jk + m_jk))
-    score -= np.sum(loggamma(a_jk))
-    return score
+    return Gp
 
 
 def main():
