445a3a35e6a194bf3163a1eb269365eedc48f78e
jint <jesse@spire.com>
2021-10-13 19:28:37 -0700

    Need to normalize input matrix so all values are index into value (i.e. indicator of k-th possible value

--- project1/project1.py
+++ project1/project1.py
@@ -32,17 +32,42 @@ def prior(n, G, r_i):
     return [np.ones((q_i[i], r_i[i]), dtype=np.uint64) for i in range(n)]
 
 
+def evidence_counts_by_ijk(n, G, r_i, D):
+    # Number of times each node, parential instantiation, and node value appears in D
+    q_i = parental_instantiations(n, G, r_i)
+    m_ijk = [np.zeros([q_i[i], r_i[i]]) for i in range(n)]
+    for row in D.itertuples():
+        for i in range(n):
+            k = row[i]
+            j = 0
+            ps = parents(i, G)
+            if len(ps) > 1:
+                parent_values = [row[i] for i in parents(i, G)]
+                parent_rs = [r_i[i] for i in parents(i, G)]
+                # parential instantiations of X_i can be thought of a w-dimensional matrix where w
+                # is the number of parents of X_i and the d-dimension can take r_i values.
+                # q_i is this flattened, so we calculate where [i_1, i_2, ..., i_h] is in flattened
+                j = np.ravel_multi_index(parent_values, parent_rs)
+            m_ijk[i][j, k] += 1
+    return m_ijk
+
+
 def _bayesian_score_for_ij(M_ij, a_ij):
     p = sum()
 
     # TODO: 
 def compute(infile, outfile):
-    df = pd.read_csv(infile, index_col=False)
-    n = len(df.columns)
-    r_i = df.nunique()
+    D = pd.read_csv(infile, index_col=False)
+    n = len(D.columns)
+    r_i = D.nunique()
 
     G = nx.DiGraph()
     G.add_nodes_from(range(n))
+
+    a = prior(n, G, r_i)
+    # TODO: Check for order of columns
+    m_ijk = evidence_counts_by_ijk(n, G, r_i, D)
+
 
     print(parents(2, G))
 
