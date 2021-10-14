206addb928ef7a2a0524b8c19124f4c4ad12f264
jint <jesse@spire.com>
2021-10-13 18:06:27 -0700

    start

--- project1/project1.py
+++ project1/project1.py
@@ -11,21 +11,27 @@ def write_gph(dag, idx2names, filename):
             f.write("{}, {}\n".format(idx2names[edge[0]], idx2names[edge[1]]))
 
 
-def prior(G, r_i):
-    n = G.number_of_nodes()
+def q_i(n, G, r_i):
+    q_i = dict((i, 1) for i in range(n))
+    # G.adjacency => {0: {}, 1: {2: {}}} where 1=>2
+    for k, children in G.adjacency():
+        for child in children.keys():
+            q_i[child] *= r_i[child]
+    print(q_i)
+
+def prior(n, G, r_i):
+    assert G.number_of_nodes() == n
 
 
 def compute(infile, outfile):
     df = pd.read_csv(infile, index_col=False)
-    # Get nodes in a form that DiGraph can easily digest (with labels)
-    # nodes = {0: {'label': 'age', 'm': 3}, 1: {'label': 'portembarked', 'm': 3}, ...
-    nodes = df.nunique().to_frame('m').reset_index().rename(columns={'index': 'label'})
-    G = nx.DiGraph()
-    G.add_nodes_from(nodes)
-    print(G.nodes[1])
+    n = len(df.columns)
     r_i = df.nunique()
 
+    G = nx.DiGraph()
+    G.add_nodes_from(range(n))
 
+    q_i(n, G, r_i)
 
 def main():
     parser = argparse.ArgumentParser()
